ca8fcad4b2a0b7f6c19f4cc858a4239ac44aecb9
Nikita Stepochkin <nikstepbox@gmail.com>
2017-10-14 20:01:45 +0300

    Add u4t4

---
+++ unit4/src/main/java/serialization/Actor.java
@@ -0,0 +1,16 @@
+package serialization;
+
+import java.io.Serializable;
+
+public class Actor implements Serializable {
+
+    private String name;
+
+    public Actor(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
